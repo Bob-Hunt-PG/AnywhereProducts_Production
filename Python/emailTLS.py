69f7b602570e16c28558c9f13b4aadc0b6828b9b
Bob Hunt <85318042+Bob-Hunt@users.noreply.github.com>
2022-02-10 13:58:33 -0500

    Updated emailTLS.py and contactform.html according to the tutorial.

--- Python/emailTLS.py
+++ Python/emailTLS.py
@@ -1,5 +1,19 @@
 import os
 import smtplib
+# import package to facilitate importing HTML form input values
+import cgi
+
+# assign variables for the form and the individual HTML form inputs
+form = cgi.FieldStorage()
+firstName = form["firstName"].value
+lastName = form["lastName"].value
+email = form["email"].value
+phone = form["phone"].value
+company = form["company"].value
+message = form["message"].value
+
+# additional variables:
+fullName = firstName + ' ' + lastName
 
 # GMAIL_USER is in the .env folder. The value is the Gmail App Code provided by the client.
 email_user=os.environ["GMAIL_USER"]
@@ -13,9 +27,22 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.starttls()
     smtp.ehlo()
     smtp.login(email_user, email_pwd)
-    subject = "Test email."
-    body = "Blah Blah Blah"
-    message = "Subject: " + subject + "\n\n" + body
-    smtp.sendmail(email_user, "bob.hunt@mailinator.com", message)
+        # subject = "Product Inquiry (website)"
+    # subject = "Product Inquiry (" + firstName + ' ' + lastName + ")"
+    subject = "Product Inquiry (" + fullName + ")"
+
+    line1 = "Subject: " + subject + "\n\n"
+    # line2 = "Name: " + firstName + ' ' + lastName + "\n\n"
+    line2 = "Name: " + fullName + "\n\n"
+    line3 = "Company: " + company + "\n\n"
+    line4 = "Phone: " + phone + "\n\n"
+    line5 = "Email: " + email + "\n\n" 
+    line6 = "Message: " + message
+    content = line1 + line2 + line3 + line4 + line5 + line6
+    # content = "Subject: " + subject + "\n\n" + "Name: " + firstName + ' ' + lastName + "\n\n" + "Company: " + company + "\n\n" + "Phone: " + phone + "\n\n" + "Email: " + email + "\n\n" +  "Message: " + message
+    # body = "Blah Blah Blah"
+    body = content
+
+    smtp.sendmail(email_user, "bob.hunt@mailinator.com", body)
 
 print('completed successfully!')
