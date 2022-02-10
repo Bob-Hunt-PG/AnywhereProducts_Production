71df229bbaa74e0e4fb9f17963a5dc666af8f399
Bob Hunt <85318042+Bob-Hunt@users.noreply.github.com>
2022-02-10 11:04:30 -0500

    Moved server2.js up one level in the Archive folder.Created a new file child_process.js

---
+++ public_html/js/child_process.js
@@ -0,0 +1,16 @@
+// The child_process module provides the ability to spawn subprocesses.
+// This capability is primarily provided by the child_process.spawn() function.
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
